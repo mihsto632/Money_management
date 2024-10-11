6b06972be756323876f18f06cdc43fe2dd08190f
mihsto632 <smihajlo133@gmail.com>
2024-10-11 13:51:57 +0000

    updated main to make 2 records (1 default and one with custom data), and add them to a database

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 
 int main(){
 
-  cout<<"Hello world!\n";
+  Trosak t1;
+  Trosak t2("Hlebac", 2024, "OktoMbar", 1850, 3310, 120000, false);
+
+  Database database;
+  database.add_record(t1);
+  database.add_record(t2);
+
+  t1.print_record();
+  t2.print_record();
 
   return 0;
 }
